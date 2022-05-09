3b552a5e265b73923e693845ad5e7bbda92faf5e
Carlos Moraga <cmoragafarias@gmail.com>
2022-05-09 12:12:06 -0400

    Added Selenium example code

--- main.py
+++ main.py
@@ -50,3 +50,54 @@ def isNumber (n):
     except (ValueError, TypeError):
         return False
 
+
+    '''
+    THIS IS SOME CODE FOR SCRAPPING WITH SELENIUM, NOT USING IT RIGHT NOW IN THE PROJECT.
+    Example of move with offset function of Selenium 
+
+    def checkCandles (driver: WebDriver, ticker):
+    
+        print (ticker)
+        tickerInputForChart = driver.find_element_by_xpath('//*[@id="trading-chart-input-symbol-1"]')
+        tickerInputForChart.clear()
+        tickerInputForChart.send_keys(ticker)
+        tickerInputForChart.send_keys(Keys.ENTER)
+        driver.execute_script('arguments[0].scrollIntoView({block: "center", inline: "center"})', tickerInputForChart)
+        time.sleep(1)
+
+
+        # Action    
+        action = webdriver.ActionChains(driver)
+
+        # get IDs
+        # //*[@id="fcMain1"]/div[2]/div/div
+        chartElements = driver.find_elements_by_xpath('//*[@id="fcMain1"]/div[2]/div/div/.//*')
+        ids = []
+        for webElement in chartElements:
+            if webElement.get_attribute('class') == 'fc-panel-box':
+                ids.append((webElement.get_attribute('id')))
+                continue
+
+        # move from last candle to first
+        chart = WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH ,'//*[@id="fcMain1"]')))
+        print (chart.size)
+        #print (chart.location)
+        action.move_to_element_with_offset(chart, 793, 285).perform()
+        position = 793        
+        while position > 0:
+            position = position - 16
+            if position < 0: break         
+            try:
+                date = WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH ,f'//*[@id="{ids[0]}"]/div[1]/div[2]')))
+                print (date.text)
+                #action.move_by_offset(-8, 0)
+                action.move_to_element_with_offset(chart, position, 285).perform()
+                action.perform()
+            except TimeoutException:
+                tickerInputForChart.clear()
+                tickerInputForChart.send_keys(ticker)
+                tickerInputForChart.send_keys(Keys.ENTER)
+                print (WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH ,f'//*[@id="{ids[0]}"]/div[1]/div[2]'))).text)
+    '''
+
+
